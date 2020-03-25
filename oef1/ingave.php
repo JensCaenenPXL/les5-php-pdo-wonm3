e587edf84c66f2e3f5a2411b47fe833707418f6f
jolien <jolien.sevenich@student.pxl.be>
2020-03-25 14:20:07 +0100

    index.html verder afgewerkt

--- oef1/ingave.php
+++ oef1/ingave.php
@@ -2,11 +2,11 @@
 $user='root';
 $password='root';
 $database='persondb';
-$pdo='null';
+$pdo=null;
 
 try {
     $pdo = new PDO("mysql:host=localhost;dbname=$database", $user, $password);
-    $statement = $pdo->prepare();
+    $statement = $pdo->prepare();                                                      //TODO : database tonen
     $statement->setFetchMode(PDO::FETCH_ASSOC);
     $statement->execute();
     var_dump($statement->fetch());
