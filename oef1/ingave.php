0cee9eacc5e97927446a7993bdc94507619e5bff
jolien <jolien.sevenich@student.pxl.be>
2020-03-25 14:15:14 +0100

    index.html body gemaakt

--- oef1/ingave.php
+++ oef1/ingave.php
@@ -3,3 +3,15 @@ $user='root';
 $password='root';
 $database='persondb';
 $pdo='null';
+
+try {
+    $pdo = new PDO("mysql:host=localhost;dbname=$database", $user, $password);
+    $statement = $pdo->prepare();
+    $statement->setFetchMode(PDO::FETCH_ASSOC);
+    $statement->execute();
+
+
+} catch (PDOException $e) {
+    print 'Exception!: ' . $e->getMessage();
+
+}
