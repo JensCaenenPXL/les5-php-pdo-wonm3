6bb2150f1fba23b09e541d0780bdc5f3bb17cfd9
zionnBE <s.smeets1@outlook.com>
2020-03-25 14:31:08 +0100

    Add ability to modify users

--- oef2/wijzig.php
+++ oef2/wijzig.php
@@ -1,1 +1,9 @@
 <?php
+include("dbconnect.php");
+
+$old_name = $_GET["old_name"];
+$new_name = $_GET["new_name"];
+$sql = "UPDATE gebruikers SET naam = ? WHERE naam = ?";
+$query = $db_conn->prepare($sql);
+$query->execute([$new_name, $old_name]);
+header("Location: overzicht.php");
