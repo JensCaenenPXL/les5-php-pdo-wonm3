d51b092fc23690bdc72bb16369b7e27cdfc51fc7
zionnBE <s.smeets1@outlook.com>
2020-03-25 13:39:52 +0100

    Init html oef2

--- oef2/overzicht.php
+++ oef2/overzicht.php
@@ -1,1 +1,37 @@
 <?php
+
+?>
+
+<html>
+<head>
+    <title>Overzicht</title>
+</head>
+<body>
+<ul>
+<?php
+    //TODO: get list
+?>
+</ul>
+<br>
+<form action="toevoegen.php">
+    <label>Voeg gebruiker toe:</label>
+    <input type="text" name="name">
+    <input type="submit" value="Voeg toe">
+</form>
+<br>
+<form action="verwijder.php">
+    <label>Verwijder gebruiker:</label>
+    <input type="text" name="name">
+    <input type="submit" value="Verwijder">
+</form>
+<form action="wijzig.php">
+    <label>Wijzig gebruiker:</label>
+    <br>
+    <label>Oude naam:</label>
+    <input type="text" name="old_name">
+    <label>Nieuwe naam:</label>
+    <input type="text" name="new_name">
+    <input type="submit" value="Wijzig">
+</form>
+</body>
+</html>
