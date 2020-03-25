2c8ef712a209fa1a5d64d7af0650a32dfb3634b8
jolien <jolien.sevenich@student.pxl.be>
2020-03-25 14:03:33 +0100

    verwerking.php geen drop, delete, truncate parameters zijn toegelaten, code toegevoegd

--- oef1/verwerking.php
+++ oef1/verwerking.php
@@ -6,6 +6,13 @@ $pdo = 'null';
 
 //TODO : uitgevoerd op voorwaarde dat er geen drop, delete of truncate in staat.
 
+if( stristr($_GET["query"] , 'drop') == true || stristr($_GET["query"] , 'delete') == true || stristr($_GET["query"] , 'truncate') == true) {
+
+    print("De opgegeven parameters zijn niet toegelaten");
+
+}else{
+
+
 try {
     $pdo = new PDO("mysql:host=localhost;dbname=$database", $user, $password);
     $statement = $pdo->query($_GET["query"]);
@@ -18,4 +25,5 @@ try {
 } catch (PDOException $e) {
     print 'Exception!: ' . $e->getMessage();
 
+}
 }
