30f5c1ef15fb961059403c9af0005126d96f4dd6
zionnBE <s.smeets1@outlook.com>
2020-03-25 14:26:32 +0100

    Add ability to remove users

--- oef2/verwijder.php
+++ oef2/verwijder.php
@@ -1,1 +1,8 @@
 <?php
+include("dbconnect.php");
+
+$naam = $_GET["name"];
+$sql = "DELETE FROM gebruikers WHERE naam = ?";
+$query = $db_conn->prepare($sql);
+$query->execute([$naam]);
+header("Location: overzicht.php");
