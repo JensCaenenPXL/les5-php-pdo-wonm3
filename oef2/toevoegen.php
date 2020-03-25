0a819b3d5562a4c163be283191ed7f3318b0c226
zionnBE <s.smeets1@outlook.com>
2020-03-25 14:23:54 +0100

    Add ability to add users

--- oef2/toevoegen.php
+++ oef2/toevoegen.php
@@ -1,1 +1,8 @@
 <?php
+include("dbconnect.php");
+
+$naam = $_GET["name"];
+$sql = "INSERT INTO gebruikers (naam) VALUES (?)";
+$query = $db_conn->prepare($sql);
+$query->execute([$naam]);
+header("Location: overzicht.php");
