668f0635a7a86f702978546474f6c6569129478d
jolien <jolien.sevenich@student.pxl.be>
2020-03-25 13:44:17 +0100

    verwerking.php statement toegevoegd in try blok

--- oef1/verwerking.php
+++ oef1/verwerking.php
@@ -1,16 +1,21 @@
 <?php
 
-$user='root';
-$password='root';
-$pdo='null';
+$user = 'root';
+$password = 'root';
+$pdo = 'null';
+
+//TODO : uitgevoerd op voorwaarde dat er geen drop, delete of truncate in staat.
+
+try {
+    $pdo = new PDO("mysql:host=localhost;dbname=$database", $user, $password);
+    $statement = $pdo->query();                                              //query nog invullen van html formulier
+    $statement->setFetchMode(PDO::FETCH_ASSOC);
+    $statement->execute();
 
 
-try{
-    $pdo = new PDO("mysql:host=localhost;dbname=$database", $user,$password);
+    //TODO : via while loop afdrukken
 
-
-
-}catch(PDOException $e){
+} catch (PDOException $e) {
     print 'Exception!: ' . $e->getMessage();
 
 }
